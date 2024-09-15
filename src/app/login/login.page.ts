f501b62acc7a8358f129ec2ef5c300001277cbfd
AdrianaAvila1710p <127752945+AdrianaAvila1710p@users.noreply.github.com>
2024-09-14 21:08:26 -0300

    first commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login', 
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private alertController: AlertController) {}
+
+  async login() {
+    if (this.username === 'Usuario1' && this.password === 'MiClav3') {
+      this.router.navigate(['/home']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Datos incorrectos.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+  }
+}
