993b04fe2d9493fb33a2440309be05447708664f
AdrianaAvila1710p <127752945+AdrianaAvila1710p@users.noreply.github.com>
2024-09-14 21:20:50 -0300

    página uno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },  {
+    path: 'uno',
+    loadChildren: () => import('./uno/uno.module').then( m => m.UnoPageModule)
+  },
+
 ];
 
 @NgModule({
