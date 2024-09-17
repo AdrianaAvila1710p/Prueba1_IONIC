df25ad250b6954d410e4191ae40c222aa9f0df7c
AdrianaAvila1710p <127752945+AdrianaAvila1710p@users.noreply.github.com>
2024-09-16 21:26:56 -0300

    Datos para poder cambiar clave

--- src/app/cambiar-clave/cambiar-clave.page.ts
+++ src/app/cambiar-clave/cambiar-clave.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cambiar-clave',
@@ -7,9 +8,78 @@ import { Router } from '@angular/router';
   styleUrls: ['./cambiar-clave.page.scss'],
 })
 export class CambiarClavePage {
-  constructor(private router: Router) {}
+  currentPassword: string = '';
+  newPassword: string = '';
+  confirmNewPassword: string = '';
+
+  constructor(private alertController: AlertController, private router: Router) {}
 
   goToMenu() {
     this.router.navigate(['/home']); 
+  }
+
+  async changePassword() {
+    // Verificar que los campos no estén vacíos
+    if (!this.currentPassword) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Debe ingresar la contraseña actual.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      this.clearFields(); // Limpiar campos
+      return;
+    }
+
+    if (!this.newPassword) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Debe ingresar la nueva contraseña.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      this.clearFields(); // Limpiar campos
+      return;
+    }
+
+    if (!this.confirmNewPassword) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Debe confirmar la nueva contraseña.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      this.clearFields(); // Limpiar campos
+      return;
+    }
+
+    // Verificar que las contraseñas nuevas coincidan
+    if (this.newPassword !== this.confirmNewPassword) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Las contraseñas no coinciden.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      this.clearFields(); // Limpiar campos
+      return;
+    }
+
+    // Simulación de un cambio de contraseña exitoso
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'La contraseña ha sido cambiada.',
+      buttons: ['OK']
+    });
+    await alert.present();
+    this.clearFields(); // Limpiar campos después de un cambio exitoso
+    this.goToMenu(); // Regresar al menú
+  }
+
+  // Método para limpiar los campos
+  clearFields() {
+    this.currentPassword = '';
+    this.newPassword = '';
+    this.confirmNewPassword = '';
   }
 }
